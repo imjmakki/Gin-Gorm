da336df9dea4c090b825965f11173076656b5a1c
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 01:54:09 +0300

    Gin Gorm

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -1,7 +1,8 @@
 package dto
 
 type UserUpdateDTO struct {
-	ID    uint64 `json:"id" from:"id" binding:"required"`
-	Name  string `json:"name" from:"name" binding:"required"`
-	Email string `json:"email" from:"email" binding:"required"`
+	ID       uint64 `json:"id" from:"id" binding:"required"`
+	Name     string `json:"name" from:"name" binding:"required"`
+	Email    string `json:"email" from:"email" binding:"required"`
+	Password string `json:"password,omitempty" from:"password,omitempty"`
 }
