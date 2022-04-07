e2711b8acd42fc0a765e09075c58867d32e5ea83
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 00:19:19 +0300

    Gin Gorm

--- config/db-config.go
+++ config/db-config.go
@@ -12,4 +12,7 @@ func DbConnect() *gorm.DB {
 		panic("Failed to load the environment file")
 	}
 	User := os.Getenv("DB_USER")
+	Password := os.Getenv("DB_PASS")
+	Host := os.Getenv("DB_HOST")
+	Name := os.Getenv("DB_NAME")
 }
