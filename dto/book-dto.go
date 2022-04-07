ac1a4e63f3c55738e42771bbcf6da54cfb8a05ee
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 01:48:00 +0300

    Gin Gorm

--- dto/book-dto.go
+++ dto/book-dto.go
@@ -1,4 +1,11 @@
 package dto
 
 type BookUpdateDTO struct {
+	ID          uint64 `json:"id" from:"id" binding:"required"`
+	Title       uint64 `json:"title" from:"title" binding:"required"`
+	Description uint64 `json:"description" from:"description" binding:"required"`
+	UserID      uint64 `json:"user_id,omitempty" from:"user_id,omitempty"`
+}
+
+type BookCreateDTO struct {
 }
