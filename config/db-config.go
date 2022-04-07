396303b1ee2e120931f8785a441f410fa112607e
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 00:17:20 +0300

    Gin Gorm

--- config/db-config.go
+++ config/db-config.go
@@ -1,7 +1,15 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+	"os"
+)
 
 func DbConnect() *gorm.DB {
-
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load the environment file")
+	}
+	User := os.Getenv("DB_USER")
 }
