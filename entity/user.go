49c3e0105555a206288aea6bd775b9b91e8f0bbe
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 00:38:36 +0300

    Gin Gorm

--- entity/user.go
+++ entity/user.go
@@ -1,5 +1,7 @@
 package entity
 
 type User struct {
-	ID uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	ID    uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	Name  string `gorm:"type:varchar(255)" json:"name"`
+	Email string `gorm:"uniqueIndex;type:varchar(255)" json:"email"`
 }
