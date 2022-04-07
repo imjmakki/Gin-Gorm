4e9d72f55e116a38a1d9fc9ea4adc8503df68e83
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 02:13:28 +0300

    Gin Gorm

--- service/jwt-service.go
+++ service/jwt-service.go
@@ -1,4 +1,14 @@
 package service
 
 type JWTService interface {
+	GenerateToken(userID string) string
+	ValidateToken(token string) (*jwt.token, error)
+}
+
+type jwtCustomClaim struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+type jwtService struct {
 }
