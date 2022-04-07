3c4570d750c0037959dae3bcc464f8cc23d7c33c
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 01:48:43 +0300

    Gin Gorm

--- dto/book-dto.go
+++ dto/book-dto.go
@@ -2,13 +2,13 @@ package dto
 
 type BookUpdateDTO struct {
 	ID          uint64 `json:"id" from:"id" binding:"required"`
-	Title       uint64 `json:"title" from:"title" binding:"required"`
-	Description uint64 `json:"description" from:"description" binding:"required"`
+	Title       string `json:"title" from:"title" binding:"required"`
+	Description string `json:"description" from:"description" binding:"required"`
 	UserID      uint64 `json:"user_id,omitempty" from:"user_id,omitempty"`
 }
 
 type BookCreateDTO struct {
-	Title       uint64 `json:"title" from:"title" binding:"required"`
-	Description uint64 `json:"description" from:"description" binding:"required"`
+	Title       string `json:"title" from:"title" binding:"required"`
+	Description string `json:"description" from:"description" binding:"required"`
 	UserID      uint64 `json:"user_id,omitempty" from:"user_id,omitempty"`
 }
