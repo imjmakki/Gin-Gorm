9ba5df3922829b7c42c4e51b9d641ece6d0af09e
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 00:58:12 +0300

    Gin Gorm

--- helper/response.go
+++ helper/response.go
@@ -1,4 +1,8 @@
 package helper
 
 type Response struct {
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Error   interface{} `json:"errors"`
+	Data    interface{} `json:"data"`
 }
