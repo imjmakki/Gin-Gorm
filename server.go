17e0fd83b8bbcd6327716959c80f87da7a331198
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 01:25:28 +0300

    Gin Gorm

--- server.go
+++ server.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"GoRm-Gin/API"
+	"GoRm-Gin/config"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db      *gorm.DB    = config.DbConnect()
+	authApi API.AuthApi = API.NewAuthApi()
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
+
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login")
+	}
+
 	r.Run()
 }
