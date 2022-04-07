b205e12e56fcf2f5d3a1ef2683b17424d40f72dd
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 02:00:13 +0300

    Gin Gorm

--- dto/login-dto.go
+++ dto/login-dto.go
@@ -1,1 +1,6 @@
 package dto
+
+type LoginDTO struct {
+	Email    string `json:"email" from:"email" binding:"required" validate:"email"`
+	Password string `json:"password,omitempty" from:"password,omitempty" validate:"min:8"`
+}
