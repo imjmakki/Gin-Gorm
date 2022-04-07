8ef1540ad3b8798d30c609c233e1ee722e6578c8
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 02:02:12 +0300

    Gin Gorm

--- dto/register-dto.go
+++ dto/register-dto.go
@@ -1,7 +1,7 @@
 package dto
 
 type RegisterDTO struct {
-	Name     string `json:"name" from:"name" binding:"required"`
+	Name     string `json:"name" from:"name" binding:"required" validate:"min:4"`
 	Email    string `json:"email" from:"email" binding:"required" validate:"email"`
 	Password string `json:"password,omitempty" from:"password,omitempty" validate:"min:8"`
 }
