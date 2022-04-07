2cccd7dbde91d7da88a887b732cef24aedbb2126
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-07 14:51:15 +0300

    Gin Gorm

--- server.go
+++ server.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	r := gin.Default()
+	r.GET("/", func(context *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello World!"})
+	})
 }
