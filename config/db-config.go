26564a2725802559458ba9dfd8cf31213e2be2ee
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 00:26:05 +0300

    Gin Gorm

--- config/db-config.go
+++ config/db-config.go
@@ -1,18 +1,26 @@
 package config
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"os"
 )
 
 func DbConnect() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
 		panic("Failed to load the environment file")
 	}
 	User := os.Getenv("DB_USER")
 	Password := os.Getenv("DB_PASS")
 	Host := os.Getenv("DB_HOST")
 	Name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:8889)/charset=utf8&parseTime=True&loc=local")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to Connect to the Database")
+	}
 }
