1e571b2639e331e72e84b29dfdf1fed22c650bcb
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 00:48:53 +0300

    Gin Gorm

--- entity/book.go
+++ entity/book.go
@@ -4,4 +4,5 @@ type Book struct {
 	ID          uint64 `gorm:"primary_key:auto_increment" json:"id"`
 	Title       string `gorm:"type:varchar(255)" json:"title"`
 	Description string `gorm:"type:text" json:"description"`
+	UserID      uint64 `gorm:"not null" json:"-"`
 }
